c261bfbc6b3d37d77e1b32a2bf03eb8e04a28793
caopeng <caopeng@skyworth.com>
2020-12-16 14:46:57 +0800

    add watcher ssh.json config

--- src/extension.ts
+++ src/extension.ts
@@ -11,30 +11,80 @@ function getConfigPath(basePath: string) {
   return path.join(basePath, CONFIG_PATH);
 }
 
-async function setup(): Promise<any> {
+function readConfig(): any {
   const workspaceFolders = vscode.workspace.workspaceFolders;
   if (!workspaceFolders) {
-    return;
+    return undefined;
   }
   const configPath = getConfigPath(workspaceFolders[0].uri.fsPath);
-  return await fse.readJson(configPath);
+  if (fse.pathExistsSync(configPath)) {
+    return fse.readJSONSync(configPath);
+  }
+  return undefined;
 }
 
 function getSshCommand(
   config: { host: string; port: number; username: string },
-  extraOpiton?: string
+  extraOption?: string
 ) {
   let sshStr = `ssh -t ${config.username}@${config.host} -p ${config.port}`;
-  if (extraOpiton) {
-    sshStr += ` ${extraOpiton}`;
+  if (extraOption) {
+    sshStr += ` ${extraOption}`;
   }
   return sshStr;
 }
 
+function isValidFile(uri: vscode.Uri) {
+  return uri.scheme === "file";
+}
+
+function isConfigFile(uri: vscode.Uri) {
+  const filename = path.basename(uri.fsPath);
+  return filename === CONFIG_FILENAME;
+}
+
+function isInWorkspace(filepath: string) {
+  const workspaceFolders = vscode.workspace.workspaceFolders;
+  return (
+    workspaceFolders &&
+    workspaceFolders.some(
+      // vscode can't keep filepath's stable, covert them to toLowerCase before check
+      (folder) =>
+        filepath.toLowerCase().indexOf(folder.uri.fsPath.toLowerCase()) === 0
+    )
+  );
+}
+
+let watcher: vscode.FileSystemWatcher;
 export async function activate(context: vscode.ExtensionContext) {
-  const remoteConfig = await setup();
+  console.log("activate");
+  let remoteConfig = readConfig();
+
+  const folder = vscode.workspace.workspaceFolders?.[0];
+  if (folder) {
+    watcher = vscode.workspace.createFileSystemWatcher(
+      new vscode.RelativePattern(folder, CONFIG_PATH),
+      false,
+      false,
+      false
+    );
+    watcher.onDidChange(function (event) {
+      remoteConfig = readConfig();
+    });
+    watcher.onDidCreate(function (event) {
+      remoteConfig = readConfig();
+    });
+    watcher.onDidDelete(function (event) {
+      remoteConfig = undefined;
+    });
+  }
+
   // args from tasks.json inputs args
   let open = vscode.commands.registerCommand("ssh-terminal.open", (args) => {
+    if (remoteConfig == undefined) {
+      vscode.window.showErrorMessage("Please create ssh.json in .vscode ");
+      return;
+    }
     var terminal = vscode.window.terminals.find(
       (bash) => bash.name == remoteConfig.name
     );
@@ -62,12 +112,17 @@ export async function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(open);
 
   let run = vscode.commands.registerCommand("ssh-terminal.run", (args) => {
+    if (remoteConfig == undefined) {
+      vscode.window.showErrorMessage("Please create ssh.json in .vscode ");
+      return;
+    }
+
     let cmd = args;
     if (args == undefined) {
       cmd = remoteConfig.command;
     }
     if (cmd == undefined) {
-      vscode.window.showInformationMessage("No found command");
+      vscode.window.showErrorMessage("No found command");
       return;
     }
 
@@ -76,15 +131,17 @@ export async function activate(context: vscode.ExtensionContext) {
     );
 
     if (bash == undefined) {
-      vscode.window.showInformationMessage(
-        "Please run Open SSH-Terminal first"
-      );
+      vscode.window.showErrorMessage("Please run Open SSH-Terminal first");
       return;
     }
     bash.sendText(cmd);
   });
 
   let close = vscode.commands.registerCommand("ssh-terminal.close", () => {
+    if (remoteConfig == undefined) {
+      vscode.window.showErrorMessage("Please create ssh.json in .vscode ");
+      return;
+    }
     var bash = vscode.window.terminals.find(
       (bash) => bash.name == remoteConfig.name
     );
@@ -96,4 +153,7 @@ export async function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  console.log("deactivate");
+  watcher?.dispose();
+}
